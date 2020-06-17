24da8e6903a74bfdf806ef13f0732bb208a1f6c7
woosung <asdota@naver.com>
2020-06-17 12:21:38 +0900

    First commit

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express')
 
+var path = require('path')
+const { request } = require('http')
+const { response } = require('express')
+
 var app = express()
 
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.get('/hello', (request , response)=>{
+    response.render('hello.ejs')
+})
 var port =8000;
-app.listen(8080, function(){
+app.listen(port, ()=>{
     console.log('server is runing at http:localhost:8080')
 })
